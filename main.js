a82c9671b86ead8366ab4ae5a53ef1df047a8667
Yuriy Kim <kimskiy@mail.ru>
2023-04-11 12:22:35 +0500

    modified css

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const slideEls = document.querySelectorAll('.slider-main__list--item');
-const slideDotsEls = document.querySelectorAll('.slider-main__dots--item');
 const dots = document.querySelectorAll('.slider-main__dots--item');
 const sliderLine = document.querySelector('.slider-main__list');
 let dotIndex = 0;
@@ -22,3 +21,22 @@ const syncDots = (index) => {
   }
   dots[index].classList.add('active-dot');
 }
+
+const sliderEl = document.querySelector('.slider-main');
+const buttonElShow = document.querySelector('.slider-main__button');
+const buttonVectorEl = document.querySelector('.slider-main__button--vector');
+const spanEl = document.querySelector('.slider-main__button--text')
+
+
+console.log(buttonElShow);
+
+buttonElShow.addEventListener('click', event => {
+  buttonVectorEl.classList.toggle('rotate-vector');
+  if (spanEl.innerText == "Показать все") {
+    sliderEl.style.maxHeight = 312 + 'px';
+    spanEl.innerText = "Скрыть"
+  } else {
+    spanEl.innerText = "Показать все";
+    sliderEl.style.maxHeight = 212 + 'px';
+  }
+})
